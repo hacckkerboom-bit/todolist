0acc90ce5239dfac0c5678c77279f5a17542f185
vishnu ps <hacckkerboom@gmail.com>
2026-05-24 23:00:47 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,53 @@
+let task = document.querySelector('#task');
+let addBtn = document.querySelector('.add-Btn');
+let outecontainer = document.querySelector('.items-maincontainer');
+
+let lists = ['gydgfdgfd', 'vishnu'];
+
+function add() {
+  let values = task.value.trim();
+
+  if (values === '') return;
+
+  lists.push(values);
+  task.value = '';
+  render();
+}
+
+function render() {
+  outecontainer.innerHTML = "";
+
+  lists.forEach(function (item, index) {
+    let divElem = document.createElement('div');
+    divElem.classList.add('item');
+
+    divElem.innerHTML = `
+      <h2>${item}</h2>
+      <button class="delete-Btn">❌</button>
+    `;
+
+    let btn = divElem.querySelector('.delete-Btn');
+
+    btn.addEventListener('click', function () {
+      del(index);
+    });
+
+    outecontainer.appendChild(divElem);
+  });
+}
+
+function del(index) {
+  lists.splice(index, 1);   // remove from array
+  render();                // redraw UI
+}
+
+// event triggers
+addBtn.addEventListener('click', add);
+
+task.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    add();
+  }
+});
+
+render();
